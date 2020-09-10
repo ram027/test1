22e18c30d612c6de82048f8d4af99db68a1738eb
Ram kumar <rk16013093@gmail.com>
2020-09-10 23:10:24 +0530

    Updates

---
+++ precache-manifest.9991e3a48fe364746bbc31676491232d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a9065e8fb79c417d8224849e057f9f",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "f1b85e5dc5eafff46385",
+    "url": "/test1/static/css/main.cbdfe89e.chunk.css"
+  },
+  {
+    "revision": "3d38c3b2dc7003409293",
+    "url": "/test1/static/js/2.16ef8202.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test1/static/js/2.16ef8202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b85e5dc5eafff46385",
+    "url": "/test1/static/js/main.92caf35b.chunk.js"
+  },
+  {
+    "revision": "e83c11b039552670e393",
+    "url": "/test1/static/js/runtime-main.2930ff92.js"
+  }
+]);
